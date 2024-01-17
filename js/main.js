7ecf7637762fd2aff8eefa616d2c07d2ff6f92ca
Maicon Santana <msn-maicon@hotmail.com>
2024-01-17 03:57:21 -0300

    jquery final validade

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function(){
 
     $('#telefone').mask('(00) 00000-0000')
+
     $('#cpf').mask('000.000.000-00')
+
+    $('#cep').mask('000000-000')
 
     $('form').validate ({
         rules: {
@@ -14,9 +17,15 @@ $(document).ready(function(){
             telefone: {
                 required: true
             },
+            cpf: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
         },
         messages:{
-            nome: 'Por favor, insira o seu nome'
+            nome: 'Por favor, confira os dados fornecidos'
         },
         submitHandler: function(form){
             console.log(form)
